e7e6be198e060160908409f86a67672cacaa7c28
VeraKulemina <110997836+VeraKulemina@users.noreply.github.com>
2022-10-04 20:37:47 -0600

    Add files via upload

---
+++ data.js
@@ -0,0 +1,25 @@
+const demonInfo = [
+    {
+        id:1,
+        title:'Bael',
+        description: 'Bael is ranked as the first demonic spirit of the Ars Goetia and principal king of Hell, ruling over the East, along with being the head of the infernal powers.',
+        likes: 5,
+        imageUrl: 'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/b9b6b1c7-6517-49bf-9307-594e9a3b84fe/d6gcydz-e68ef44f-8db2-4094-8e8b-8f2eb7893651.jpg/v1/fill/w_832,h_960,q_70,strp/_1_bael___baal___goetic_demons_by_willd8_d6gcydz-pre.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MTE4MiIsInBhdGgiOiJcL2ZcL2I5YjZiMWM3LTY1MTctNDliZi05MzA3LTU5NGU5YTNiODRmZVwvZDZnY3lkei1lNjhlZjQ0Zi04ZGIyLTQwOTQtOGU4Yi04ZjJlYjc4OTM2NTEuanBnIiwid2lkdGgiOiI8PTEwMjQifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.zSYpU1S0zmHXcT-DPa9U-l8PJnGUod0yvQl5BWVGtn',
+        
+    },
+    {
+        id:2,
+        title:'Aamon',
+        description: 'Aamon is a Marquis of Hell and the seventh of the 72 Goetic Demons of the Ars Goetia, he governs 40 infernal legions and is the younger brother of Amaymon. His personality primarily embodies the sin of Wrath.',
+        likes: 3,
+        imageUrl: 'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/53d77925-bd52-411e-880f-1b37fb89e425/ddfstvf-a3c62b2c-6634-4d02-9257-6c6708eb0a0c.jpg/v1/fill/w_495,h_700,q_70,strp/aatrox_by_hoken45_ddfstvf-350t-2x.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MjI2MyIsInBhdGgiOiJcL2ZcLzUzZDc3OTI1LWJkNTItNDExZS04ODBmLTFiMzdmYjg5ZTQyNVwvZGRmc3R2Zi1hM2M2MmIyYy02NjM0LTRkMDItOTI1Ny02YzY3MDhlYjBhMGMuanBnIiwid2lkdGgiOiI8PTE2MDAifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.ZAK-6AyZEXOmX6iyruXHFtQ4B9f4obcUgOAKSCqbdAo',
+        
+    },
+    {
+        id:3,
+        title:'Asmodeus',
+        description: 'His form as an Angel is Asmodel, the Angel of April and Patience who rules the Zodiacal sign of Taurus. Asmodeus also has a child or clone also called Asmodeus though is usually called Asmoday. It is said that for people who fall under Asmodeus ways will be sentenced to an eternity in the second level of Hell.',
+        likes: 9,
+        imageUrl: 'https://st.depositphotos.com/1695244/52729/i/450/depositphotos_527290846-stock-photo-fantasy-demon-devil-leather-pants.jpg',
+        
+    }, ]
--- index.js
+++ index.js
@@ -1,34 +1,47 @@
-const test = document.querySelector("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", () => {
     fetch("https://rickandmortyapi.com/api/character/1,2,3,4,5")
       .then(response => response.json())
       .then(characters => {
-        characters.forEach(objCharacters => showCharacter(objCharacters))
-        displayCharacters(characters[0])
-      })
+          characters.forEach(objCharacters => showCharacter(objCharacters))
+          displayCharacters(characters[0])
+        })})
+
+const characterBar = document.getElementById("character-bar");
+const charactersImage = document.querySelector("#image");
+const cuteNames = document.querySelector("#name");
+const statusChar = document.querySelector("#vote-count")
+characterBar.addEventListener("mouseover", (event) => {
+    // highlight the mouseover target
+    event.target.style.color = "red";
 });
-
-const characterBar = document.getElementById("#character-bar");
-const charactersImage = document.querySelector('#image');
-const cuteNames = document.querySelector('#name');
-const statusChar = document.querySelector("vote-count");
-
 characterBar.addEventListener("mouseout", (event) => {
-    event.target.style.color = "red"
+    // highlight the mouseover target
+    event.target.style.color = "#78BE21";
 });
-
-characterBar.addEventListener("mouseout", (event) => {
-    event.target.style.color = "#78BE21"
-});
-
-function displayCharacters(objCharacters) {
-    cuteNames.textContent = objCharacters.name;
-    charactersImage.src = objCharacters.image;  
-    charactersImage.src= objCharacters.image;
-}
-
 function showCharacter(objCharacter) {
-    const img = document.createElement('span');
+    const img = document.createElement("span");
     img.textContent = objCharacter.name;
-    img.addEventListener('click', () => displayCharacters(objCharacter))
+    img.addEventListener("click", (e) => displayCharacters(objCharacter))
     characterBar.append(img);
- }
+}
+function displayCharacters(objCharacters) {
+    //console.log(characters)
+    cuteNames.textContent = objCharacters.name
+    statusChar.textContent = objCharacters.status;
+   charactersImage.src = objCharacters.image
+}
+document.querySelector("#votes-form").addEventListener("submit", (e) =>{
+    e.preventDefault();
+    let copyVotes = e.target.votes.value
+    console.log(copyVotes);
+    handleVotes(copyVotes);
+})
+function handleVotes(votesAdded, currentVotes = parseInt(document.querySelector("#vote").innerText)){
+    currentVotes += parseInt(votesAdded);
+    let voteDisplay = document.querySelector("#vote");
+    voteDisplay.textContent = currentVotes;
+    console.log(currentVotes);
+}
+document.querySelector("#reset-btn").addEventListener("click", (event) => {
+    handleVotes(0, 0);
+})
